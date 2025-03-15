d2e39ee6361d825d42d9919db995f528136d89ea
Jimmy Lucero Vasquez <jim.octavio2016@gmail.com>
2025-03-14 21:47:17 -0500

    Se estilizó el encabezo y pie de página. Además se asignaron clases para cambiar el estilo de los enlaces con js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Lógica para cambiar la clase de los enlaces a RRSS cuando hacemos click sobre ellos
+// Lógica para cambiar la clase de los enlaces de RRSS cuando hacemos click sobre ellos
 document.querySelectorAll(".contenido__presentacion__redSocial__item").forEach(item => {
     // Agregar la clase --hover cuando el usuario pasa el cursor
     item.addEventListener("mouseenter", function () {
@@ -12,11 +12,26 @@ document.querySelectorAll(".contenido__presentacion__redSocial__item").forEach(i
 
     // Agregar la clase --active cuando el usuario hace clic
     item.addEventListener("mousedown", function () {
+        document.querySelectorAll(".contenido__presentacion__redSocial__item--active").forEach(activeItem => activeItem.classList.remove("contenido__presentacion__redSocial__item--active"));
         this.classList.add("contenido__presentacion__redSocial__item--active");
     });
+});
 
-    // Remover la clase --active cuando el usuario suelta el clic
+// Lógica para cambiar la clase de los enlaces del menu cuando hacemos click sobre ellos
+document.querySelectorAll(".encabezado__menu__item").forEach(item => {
+    // Agregar la clase --hover cuando el usuario pasa el cursor
+    item.addEventListener("mouseenter", function () {
+        this.classList.add("encabezado__menu__item--hover");
+    });
+
+    // Remover la clase --hover cuando el cursor sale
     item.addEventListener("mouseleave", function () {
-        this.classList.remove("contenido__presentacion__redSocial__item--active");
+        this.classList.remove("encabezado__menu__item--hover");
+    });
+
+    // Agregar la clase --active cuando el usuario hace clic
+    item.addEventListener("mousedown", function () {
+        document.querySelectorAll(".encabezado__menu__item--active").forEach(activeItem => activeItem.classList.remove("encabezado__menu__item--active"));
+        this.classList.add("encabezado__menu__item--active");
     });
 });
